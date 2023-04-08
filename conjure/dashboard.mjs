7487a0a6f86b6bcf003e729519dc258b6d08d39d
John Vinyard <johnwho@gmail.com>
2023-04-07 21:24:09 -0500

    Get basic JS interaction with keys working

--- conjure/dashboard.mjs
+++ conjure/dashboard.mjs
@@ -16,7 +16,7 @@ camera.position.set(1, 1, 1);
 camera.lookAt(scene.position);
 
 const renderer = new THREE.WebGLRenderer({ canvas: myCanvas });
-renderer.setClearColor(0xffffff, 1.0); 
+renderer.setClearColor(0xffffff, 1.0);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(myCanvas.offsetWidth, myCanvas.offsetHeight);
 
@@ -24,8 +24,8 @@ const orbitControls = new OrbitControls(camera, renderer.domElement);
 orbitControls.maxPolarAngle = Math.PI * 0.5;
 orbitControls.minDistance = 0.1;
 orbitControls.maxDistance = 100;
-orbitControls.autoRotate = true; 
-orbitControls.autoRotateSpeed = 1.0; 
+orbitControls.autoRotate = true;
+orbitControls.autoRotateSpeed = 1.0;
 
 renderer.setAnimationLoop(() => {
   orbitControls.update();
@@ -33,47 +33,52 @@ renderer.setAnimationLoop(() => {
   renderer.render(scene, camera);
 });
 
-// document.addEventListener(
-//   "DOMContentLoaded",
-//   async () => {
-//     console.log("READY");
+const fetchData = async (key) => {
+  const resp = await fetch(`/results/${key}`);
+  console.log(resp);
+};
 
-//     let offset = "";
+document.addEventListener(
+  "DOMContentLoaded",
+  async () => {
+    console.log("READY");
 
-//     setInterval(() => {
-//       console.log(`fetching with timestamp ${offset}`);
-//       fetch(
-//         "/feed?" +
-//           new URLSearchParams({
-//             offset,
-//           })
-//       ).then(async (resp) => {
-//         const data = await resp.json();
-//         const list = document.getElementById("new");
-//         data.forEach(({ key, timestamp }) => {
-//           list.insertAdjacentHTML(
-//             "afterbegin",
-//             `<li><a target="_blank" href="/results/${key}">${key}</a></li>`
-//           );
-//         });
-//         if (data.length > 0) {
-//           offset = data.slice(-1)[0].timestamp;
-//           console.log(`Updated timestamp to be ${offset}`);
-//         }
-//       });
-//     }, 2500);
+    let offset = "";
 
-//     // list the keys
-//     const results = await fetch("/");
-//     const data = await results.json();
-//     const keysList = document.getElementById("keys");
-//     keysList.innerHTML = "";
-//     data.forEach((key) => {
-//       keysList.insertAdjacentHTML(
-//         "afterbegin",
-//         `<li><a target="_blank" href="/results/${key}">${key}</a></li>`
-//       );
-//     });
-//   },
-//   false
-// );
+    // setInterval(() => {
+    //   console.log(`fetching with timestamp ${offset}`);
+    //   fetch(
+    //     "/feed?" +
+    //       new URLSearchParams({
+    //         offset,
+    //       })
+    //   ).then(async (resp) => {
+    //     const data = await resp.json();
+    //     const list = document.getElementById("new");
+    //     data.forEach(({ key, timestamp }) => {
+    //       list.insertAdjacentHTML(
+    //         "afterbegin",
+    //         `<li><a target="_blank" href="/results/${key}">${key}</a></li>`
+    //       );
+    //     });
+    //     if (data.length > 0) {
+    //       offset = data.slice(-1)[0].timestamp;
+    //       console.log(`Updated timestamp to be ${offset}`);
+    //     }
+    //   });
+    // }, 2500);
+
+    // list the keys
+    const results = await fetch("/");
+    const data = await results.json();
+    const keysList = document.getElementById("keys");
+    keysList.innerHTML = "";
+    data.forEach((key) => {
+      const li = document.createElement("li");
+      li.innerText = key;
+      li.onclick = () => fetchData(key);
+      keysList.appendChild(li);
+    });
+  },
+  false
+);
