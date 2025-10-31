6f291a7404b8575ea01685f5b95f51914ee3157e
John Vinyard <johnwho@gmail.com>
2025-10-31 11:08:55 -0500

    Increase default map size, since there is no penalty for making it huge

--- conjure/storage.py
+++ conjure/storage.py
@@ -221,7 +221,7 @@ class LmdbCollection(Collection):
         self.env = lmdb.open(
             self.path,
             max_dbs=10,
-            map_size=10e10,
+            map_size=10e11, # one terabyte
             writemap=True,
             map_async=True,
             metasync=True,
